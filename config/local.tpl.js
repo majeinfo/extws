6d6674180fb2e6557682795396a64dea12849325
JD <jd@maje.biz>
2016-08-24 08:54:43 +0200

    influx

--- config/local.tpl.js
+++ config/local.tpl.js
@@ -8,4 +8,11 @@ exports.mongoDB = 'domopi';
 exports.mongoUser = 'domopi';
 exports.mongoPassword = 'domopi';
 
+exports.use_influxdb = true;
+exports.influxSrv = 'localhost';
+exports.influxPort = 8086;
+exports.influxDB = 'domopi';
+exports.influxUser = '';
+exports.influxPassword = '';
+
 // EOF
